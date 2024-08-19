2a417455c1dcdea39abe0517e41a291b2786b810
K0ndrich <123kondrich@gmail.com>
2024-08-19 19:51:35 +0300

    edited

--- games/heads_and_tails.py
+++ games/heads_and_tails.py
@@ -30,11 +30,8 @@ def play_martingale(*, starting_funds: int, min_bet: int, max_bet: int):
 
     while current_funds > 0:
 
-        print("---------------------------------------------------")
         steps_to_loose += 1
         current_funds -= current_bet
-
-        print(f"current_funds -> {current_funds} , current_bet -> {current_bet}")
 
         flipped_coin_value = flip_coin()
 
@@ -42,7 +39,6 @@ def play_martingale(*, starting_funds: int, min_bet: int, max_bet: int):
         if flipped_coin_value == HEADS:
 
             win = current_bet * 2
-            print(f"You win -> {win}")
 
             # отдаем пользователю выигрыш
             current_funds += win
@@ -51,9 +47,6 @@ def play_martingale(*, starting_funds: int, min_bet: int, max_bet: int):
 
         # тут игрок проигрывает
         else:
-
-            print(f"- You loose -")
-
             # текущая ставка увеличиваеться в два раза
             current_bet *= 2
 
@@ -68,4 +61,28 @@ def play_martingale(*, starting_funds: int, min_bet: int, max_bet: int):
     return steps_to_loose
 
 
-print(play_martingale(starting_funds=100, min_bet=2, max_bet=4))
+# играем одним игроком
+play_martingale(starting_funds=100, min_bet=2, max_bet=4)
+
+
+# играет указанное количество игроков
+def simulate_martingale_for_n_player(
+    *, starting_funds: int, min_bet: int, max_bet: int, n_games: int
+):
+    # количество все шагов всех игроков до проигрыша
+    total_steps_to_loose = 0
+
+    for i in range(n_games):
+        # количество шагов одного игрока до проигрыша
+        step_to_loose = play_martingale(
+            starting_funds=starting_funds, min_bet=min_bet, max_bet=max_bet
+        )
+        total_steps_to_loose += step_to_loose
+
+    return total_steps_to_loose / n_games
+
+
+# играем несолькими игроками
+simulate_martingale_for_n_player(
+    n_games=10, starting_funds=1000, min_bet=1, max_bet=100
+)
