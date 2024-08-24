33c764155391096da69ef6bfc93d929868b87b9b
K0ndrich <123kondrich@gmail.com>
2024-08-24 12:38:04 +0300

    edited

---
+++ requests/binance.py
@@ -0,0 +1,45 @@
+# Библиотека requests
+
+import requests
+import time
+
+
+# url-адресс api bincance
+url = "https://api.binance.com/api/v3/ticker/price"
+
+# получем ответ от binance про стоимость биткоина к долару
+response = requests.get(url, params={"symbol": "BTCUSDT"})
+
+
+# данные в ответе от binance храняться в response.content
+content = response.content
+
+# возвращаеться json-строка
+content  # -> b'{"symbol":"BTCUSDT","price":"64220.00000000"}'
+type(content)  # -> <class 'bytes'>
+
+
+# форматируем все полученные данные json-формата, кототорые лежат в content в данные типа словаря dict
+price = response.json()
+
+price  # -> {'symbol': 'BTCUSDT', 'price': '64243.00000000'}
+type(price)  # -> <class 'dict'>
+
+
+# цены на биткоины за текущие 10 секунд
+bitcoin_prices = []
+for i in range(10):
+    response = requests.get(url, params={"symbol": "BTCUSDT"})
+    # переводим в словарь только значение ключа price
+    price = float(response.json()["price"])
+    # добавляем елементв в список
+    bitcoin_prices.append(price)
+    # остановление выполнение кода на 1 секунду
+    time.sleep(1)
+
+
+bitcoin_prices  # -> [64291.23, 64291.22, 64291.22, 64291.23, 64291.22, 64291.22, 64291.23, 64291.23, 64291.22, 64291.23]
+type(bitcoin_prices)  # -> <class 'list'>
+len(bitcoin_prices)  # -> 10
+max(bitcoin_prices)  # -> 64291.23
+min(bitcoin_prices)  # -> 64291.22
