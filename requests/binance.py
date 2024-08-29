f885f14f5b6ffe212da4e8eafd5fe96743bc1934
K0ndrich <123kondrich@gmail.com>
2024-08-29 13:23:53 +0300

    edited

--- requests/binance.py
+++ requests/binance.py
@@ -7,9 +7,9 @@ import time
 # url-адресс api bincance
 url = "https://api.binance.com/api/v3/ticker/price"
 
+
 # получем ответ от binance про стоимость биткоина к долару
 response = requests.get(url, params={"symbol": "BTCUSDT"})
-
 
 # данные в ответе от binance храняться в response.content
 content = response.content
@@ -17,7 +17,6 @@ content = response.content
 # возвращаеться json-строка
 content  # -> b'{"symbol":"BTCUSDT","price":"64220.00000000"}'
 type(content)  # -> <class 'bytes'>
-
 
 # форматируем все полученные данные json-формата, кототорые лежат в content в данные типа словаря dict
 price = response.json()
@@ -28,14 +27,23 @@ type(price)  # -> <class 'dict'>
 
 # цены на биткоины за текущие 10 секунд
 bitcoin_prices = []
-for i in range(10):
-    response = requests.get(url, params={"symbol": "BTCUSDT"})
-    # переводим в словарь только значение ключа price
-    price = float(response.json()["price"])
-    # добавляем елементв в список
-    bitcoin_prices.append(price)
-    # остановление выполнение кода на 1 секунду
-    time.sleep(1)
+
+# ловим исключение при ошибке подключиться к указаному сайту
+try:
+    for i in range(10):
+        response = requests.get(url, params={"symbol": "BTCUSDT"})
+        # переводим в словарь только значение ключа price
+        price = float(response.json()["price"])
+        # добавляем елементв в список
+        bitcoin_prices.append(price)
+        # остановление выполнение кода на 1 секунду
+        time.sleep(1)
+
+except requests.exceptions.ConnectionError as error:
+    print(
+        f"Something goes wrong: {error}"
+    )  # -> HTTPSConnectionPool(host='api.binance.com', port=443): Max retries exceeded with url: /api/v3/ticker/price?symbol=BTCUSDT
+    print("There is NO internet")
 
 
 bitcoin_prices  # ->  [64291.23, 64291.22, 64291.22, 64291.23, 64291.22, 64291.22, 64291.23, 64291.23, 64291.22, 64291.23]
