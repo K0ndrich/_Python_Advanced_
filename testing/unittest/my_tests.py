b44bce416621592ed02f121da7338601adb02f0b
K0ndrich <123kondrich@gmail.com>
2024-10-02 18:39:55 +0300

    edited

--- testing/unittest/my_tests.py
+++ testing/unittest/my_tests.py
@@ -2,8 +2,33 @@
 # Успешные Тесты - ето гарантия и доказательство, что написаный код правильный и рабочий
 
 
-# функция калькулятор
-def calculator(expression):
+# функция калькулятор, которая принимает агрумент строку, где есть и числа и математическая операция которую нужно выполнить с ними
+def calculator(expression: str):
     # содержит допустимые операции + - / * с числами внутри текущей функции
-    allowed = ="+-/*"
-    
+    allowed = "+-/*"
+    # если нету ни одного разрешеного знака в переданом аргументе, тогда выбрасываем ошибку
+    # sign ето математическая разрешеная операция "+-/*"
+    if not any(sign in expression for sign in allowed):
+        raise ValueError(
+            f"Expression must have at least one allowed math operator {allowed}"
+        )
+
+    for sign in allowed:
+        if sign in expression:
+            try:
+                # делим весь переданый аргумент по математическому знаку "+-/*" на два числа
+                left, right = expression.split(sign)
+                left, right = int(left), int(right)
+
+                if sign == "+":
+                    return left + right
+                elif sign == "-":
+                    return left - right
+                elif sign == "*":
+                    return left * right
+                elif sign == "/":
+                    return left / right
+            except (ValueError, TypeError):
+                raise ValueError(
+                    f"Expression must have two integer numbers and one math operator {allowed}"
+                )
