625dac373749602e799579b4392d5081c49c69a6
K0ndrich <123kondrich@gmail.com>
2024-10-03 18:36:42 +0300

    edited

--- programs/my_clock/my_clock.py
+++ programs/my_clock/my_clock.py
@@ -20,10 +20,12 @@ class Clock:
     def __get_formated(cls, x):
         return str(x).rjust(2, "0")
 
+    # my_object + 10 реализация
     def __add__(self, other):
         if not isinstance(other, (int, Clock)):
             raise ArithmeticError("Right number must be integer number or type Clock")
 
+        # my_object + my_object реализация
         sc = other
         # если other являеться обьектов класса Clock
         if isinstance(other, Clock):
@@ -31,9 +33,34 @@ class Clock:
             sc = other.seconds
         return Clock(self.seconds + sc)
 
+    # 10 + my_object реализация
+    # __radd__ -> __add__ последовательность вызова
+    def __radd__(self, other):
+
+        return self + other
+
+    # my_object += 10 реализация, просто укороченая форма добавления значения к обьекту
+    def __iadd__(self, other):
+
+        if not isinstance(other, (int, Clock)):
+            raise ArithmeticError("Right number must be integer number or type Clock")
+
+        sc = other
+        if isinstance(other, Clock):
+            sc = other.seconds
+
+        self.seconds += sc
+
+        # возвращаем изменные тот же екзепляр, а не создаем новый екзепляр с новыми характеристиками
+        return self
+
 
 c1 = Clock(1000)
 c2 = Clock(2000)
 c3 = Clock(3000)
 с4 = c1 + c2 + c3
-print(с4.get_time())
+с4.get_time()
+
+my_object = Clock(5000)
+my_object += 10
+print(my_object.get_time())
